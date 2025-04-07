aeb8a95566786135d3a1012a81d885aed02f6067
Simpledigital <admin@simpledigital.cl>
2025-04-07 13:15:57 -0400

    Update hr_mutualidad.py

--- l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/model/hr_mutualidad.py
+++ l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/model/hr_mutualidad.py
@@ -1,9 +1,15 @@
-from odoo import api, fields, models, tools, _
+from odoo import api, fields, models, _
 
 
-class hr_mutualidad(models.Model):
+class HrMutual(models.Model):
     _name = 'hr.mutual'
-    _description = 'Mutualidad'
-    
-    codigo = fields.Char('Codigo', required=True)
-    name = fields.Char('Nombre', required=True)
+    _description = _('Mutualidad')
+    _order = 'name'
+
+    codigo = fields.Char(string='Código', required=True)
+    name = fields.Char(string='Nombre', required=True)
+
+    _sql_constraints = [
+        ('codigo_unique', 'unique(codigo)', 'El código debe ser único.'),
+        ('name_unique', 'unique(name)', 'El nombre debe ser único.'),
+    ]
