1bbdc525f96669a6d7d523538919038039961977
Simpledigital <admin@simpledigital.cl>
2025-04-07 12:25:58 -0400

    Update __init__.py

--- l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/models/__init__.py
+++ l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/models/__init__.py
@@ -1,3 +1,8 @@
-
 from . import hr_isapre
+from . import hr_afp
+from . import hr_apv
+from . import hr_ccaf
+from . import hr_seguro_complementario
+from . import hr_mutual
 from . import hr_indicadores
+from . import hr_type_employee
