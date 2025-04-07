222608b2b5b9cc8007a55d88ff64448b7c7353a9
Simpledigital <admin@simpledigital.cl>
2025-04-07 15:31:14 -0400

    Update hr_isapre.py

--- l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/models/hr_isapre.py
+++ l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/models/hr_isapre.py
@@ -3,9 +3,10 @@ from odoo import models, fields
 class HrIsapre(models.Model):
     _name = "hr.isapre"
     _description = "Isapre - Institución de Salud Previsional"
+    _order = "name"
 
     name = fields.Char(string="Nombre", required=True)
-    codigo = fields.Char(string="Código")
-    rut = fields.Char(string="RUT")
-    tasa = fields.Float(string="Tasa (%)", digits=(16, 2))
+    codigo = fields.Char(string="Código", required=True)
+    rut = fields.Char(string="RUT", required=True)
+    tasa = fields.Float(string="Tasa (%)", digits="Payroll", required=True)
     fonasa = fields.Boolean(string="¿Es Fonasa?", default=False)
