68dc14ad3c39ee74243b203bf2be55d05024d7a4
Simpledigital <admin@simpledigital.cl>
2025-04-07 13:02:02 -0400

    Update hr_afp.py

--- l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/model/hr_afp.py
+++ l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/model/hr_afp.py
@@ -1,13 +1,19 @@
-from odoo import api, fields, models, tools, _
+from odoo import api, fields, models, _
 
 
-class hr_afp(models.Model):
+class HrAfp(models.Model):
     _name = 'hr.afp'
-    _description = 'Fondos de Pension'
+    _description = _('Fondos de Pensión')
+    _order = 'name'
 
-    codigo = fields.Char('Codigo', required=True)
-    name = fields.Char('Nombre', required=True)
-    rut = fields.Char('RUT', required=True)
-    rate = fields.Float('Tasa', required=True)
-    sis = fields.Float('Aporte Empresa', required=True)
-    independiente = fields.Float('Independientes', required=True)
+    codigo = fields.Char(string='Código', required=True)
+    name = fields.Char(string='Nombre', required=True)
+    rut = fields.Char(string='RUT', required=True)
+    rate = fields.Float(string='Tasa de Cotización (%)', required=True)
+    sis = fields.Float(string='Aporte SIS Empresa (%)', required=True)
+    independiente = fields.Float(string='Independientes (%)', required=True)
+
+    _sql_constraints = [
+        ('codigo_unique', 'unique(codigo)', 'El código debe ser único.'),
+        ('rut_unique', 'unique(rut)', 'El RUT debe ser único.'),
+    ]
