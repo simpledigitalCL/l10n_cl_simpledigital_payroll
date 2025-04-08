c4df8c4481127b8e294d263dd07a11296039ad86
Simpledigital <admin@simpledigital.cl>
2025-04-08 10:47:39 -0400

    Update report_payslip.py

--- l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/report/report_payslip.py
+++ l10n_cl_simpledigital_payroll_odoo_sh_Chile/l10n_cl_simpledigital_payroll/report/report_payslip.py
@@ -1,44 +1,25 @@
 from odoo import api, models
 
 
-class payslip_report(models.AbstractModel):
+class PayslipReport(models.AbstractModel):
     _inherit = 'report.hr_payroll.report_payslipdetails'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        payslips = super(payslip_report, self)._get_report_values(docids, data)
-        #payslips.update({
-#        	'get_payslip_lines': self.get_payslip_lines(),
-#            #'convert': self.convert(),
-#            'get_leave':self.get_leave(),
-#        })
+        payslips = super(PayslipReport, self)._get_report_values(docids, data)
+        # Descomenta si quieres incluir más funciones personalizadas:
+        # payslips.update({
+        #     'get_leave': self.get_leave,
+        #     'convert': self.convert,
+        #     'get_payslip_lines': self.get_payslip_lines,
+        # })
         return payslips
 
-    @api.multi
-    def convert(self,amount, cur):
-        amt_en = cur.amount_to_text(amount)
-        return amt_en
+    def convert(self, amount, cur):
+        return cur.amount_to_text(amount)
 
-    @api.multi
-    def get_payslip_lines(self):
-        payslip_line = self.env['hr.payslip.line']
-        res = []
-        ids = []
-        for rec in self:
-            if rec.appears_on_payslip is True:
-                ids.append(rec.id)
-        if ids:
-            res = payslip_line.browse(ids)
-        return res
+    def get_payslip_lines(self, payslip_lines):
+        return payslip_lines.filtered(lambda l: l.appears_on_payslip)
 
-    @api.multi
-    def get_leave(self, obj):
-        res = []
-        ids = []
-        for rec in self:
-            if rec.type == 'leaves':
-                ids.append(rec.id)
-            payslip_line = self.env['hr.payslip.line']
-            if ids:
-                res = payslip_line.browse(ids)
-        return res
+    def get_leave(self, payslip_lines):
+        return payslip_lines.filtered(lambda l: l.type == 'leaves')
